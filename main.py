95d786c89090f0661c8575c7941f24abec567d3c
Edandaveywood <Edandaveywood@gmail.com>
2020-11-15 23:33:02 +1300

    Small final changes

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ class CommandLineInterface(Cmd):
         self.intro = "This program will generate a class diagram from your JavaScript source code. " \
                      "Type help for a list of commands."
         j_loader = JsonLoader('help_file.json')
-        try:
-            j_loader.open_file()
-        except FileNotFoundError:
-            print('There is no help file.')
+        j_loader.open_file()
         self.j_loader = j_loader
         self._input_file = None
         self._director = Director()
@@ -39,8 +36,14 @@ class CommandLineInterface(Cmd):
     def do_load_pickle(self, arg):
         self._converter.load_pickle(Pickler())
 
+    def help_load_pickle(self):
+        print(self.j_loader.get_help_text('load_pickle'))
+
     def do_remove_pickle(self, arg):
         self._converter.remove_pickle(Pickler())
+
+    def help_remove_pickle(self):
+        print(self.j_loader.get_help_text('remove_pickle'))
 
     def do_exit(self, arg):
         return True
@@ -49,46 +52,30 @@ class CommandLineInterface(Cmd):
         print(self.j_loader.get_help_text('exit'))
 
     def do_load_data(self, arg):
-        try:
-            raw_data = arg.split()
-            self._input_file = raw_data[0]
-            ReadJs().check_file_type(self._input_file)
-            self._converter.load_data(self._input_file)
-        except Exception as e:
-            print(e)
+        raw_data = arg.split()
+        self._input_file = raw_data[0]
+        ReadJs().check_file_type(self._input_file)
+        self._converter.load_data(self._input_file)
 
     def help_load_data(self):
         print(self.j_loader.get_help_text('load_data'))
 
     def do_extract_data(self, arg):
-        try:
-            self._converter.extract_data()
-            self._converter.get_dict()
-        except Exception as e:
-            print(e)
+        self._converter.extract_data()
+        self._converter.get_dict()
 
     def help_extract_data(self):
         print(self.j_loader.get_help_text('extract_data'))
 
     def do_convert_to_uml(self, arg):
-        try:
-            self._converter.convert_to_uml()
-            self._converter.uml.view_uml()
-        except Exception as e:
-            print(e)
+        self._converter.convert_to_uml()
+        self._converter.uml.view_uml()
 
     def help_convert_to_uml(self):
         print(self.j_loader.get_help_text('convert_to_uml'))
 
-    # This is outside the scope of the original tests as I added it for the use of director for a faster UML creation.
+    # This is outside the scope of the original tests as
+    # I added it for the use of director for a faster UML creation.
     def do_convert_fast(self):
         self._director.build_uml(self._input_file)
         self._converter.uml.view_uml()
-
-
-if __name__ == '__main__':
-    import sys
-
-    cli = CommandLineInterface()
-    sys_exit_code = cli.cmdloop()
-    sys.exit(sys_exit_code)
